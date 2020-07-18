940f8ee5fb149d9704517b116c8a3fc65857e98c
Abdulmatin Sanni <abdulmatin.i.sanni@gmail.com>
2020-07-18 01:50:07 +0100

    Refactor code

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import React from "react";
 import { Provider as StoreProvider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import store, { persistor } from "state/store";
-import { BrowserRouter ,Route, Switch} from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./styles/index.css";
 import Login from "pages/auth/Login";
 import SignUp from "pages/auth/signUp";
 import ForgotPassword from "pages/auth/ForgotPassword/ForgotPassword";
 import ForgotPassTwo from "pages/auth/ForgotPassword/ForgotPassTwo";
 import Dashboard from "pages/dashboard/Dashboard";
-
 
 function App() {
   // const mobileMenu= false;
@@ -24,9 +23,7 @@ function App() {
               <Route path="/signup" component={SignUp} />
               <Route path="/forgot-pass" component={ForgotPassword} />
               <Route path="/forgot-two" component={ForgotPassTwo} />
-                <Route path="/dashboard">
-                    <Dashboard  />
-                </Route>
+              <Route path="/dashboard" component={Dashboard} />
             </Switch>
           </main>
         </BrowserRouter>
