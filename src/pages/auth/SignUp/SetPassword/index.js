76db98693a9058855cbcf20757bb9ddab1bad089
Abdulmatin Sanni <abdulmatin.i.sanni@gmail.com>
2020-07-19 20:10:18 +0100

    Edit set password page

--- src/pages/auth/SignUp/SetPassword/index.js
+++ src/pages/auth/SignUp/SetPassword/index.js
@@ -6,7 +6,8 @@ import * as yup from "yup";
 import { Field, Form, Formik } from "formik";
 import { connect } from "react-redux";
 import { login } from "state/ducks/user/actions";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
+import { completeVerifyEmail } from "../../../../state/ducks/completeVerifyEmail/actions";
 
 const initialValues = {
   password: "",
@@ -22,100 +23,117 @@ const validationSchema = yup.object().shape({
     .required(),
 });
 
-const SignUpSetPassword = ({ history, loading, error, dispatchLogin }) => (
-  <section className="h-screen w-full setup-screens flex flex-col justify-center items-center leafy-bg">
-    <div className="flex flex-col items-center bg-white fadeIn login-fieldset auth-modal">
-      <div className="flex flex-col items-center setup-success mb-6">
-        <i className="w-10 mb-4">
-          <img src={Icon} alt="" />
-        </i>
-        <h1 className="text-xl font-medium mb-2">Set Password</h1>
-        <p className="text-gray-500 text-center leading-normal">
-          Enter a password that will be easy for you and difficult for others to
-          remember
-        </p>
-      </div>
+const SignUpSetPassword = ({
+  history,
+  loading,
+  error,
+  verificationID,
+  dispatchLogin,
+}) => {
+  const handleOnSubmit = (formValues, formikProps) => {
+    const payload = { ...formValues, verificationID };
+    const meta = { formikProps, history };
+    dispatchCompleteVerifyEmail(payload, meta);
+  };
 
-      {loading ? (
-        <Loading text="Verifying your password" />
-      ) : (
-        <Fragment>
-          {error && (
-            <div className="w-72 mb-8 text-xs text-left">
-              <p className="w-full p-3 bg-red-200 text-red-700 rounded text-center font-medium">
-                {error}
-              </p>
-            </div>
-          )}
+  if (!verificationID) {
+    return <Redirect to="/auth/sign-up" />;
+  }
 
-          <Formik
-            initialValues={initialValues}
-            validationSchema={validationSchema}
-            validateOnMount={true}
-            onSubmit={(formValues, formikProps) => {
-              dispatchLogin(formValues, { formikProps, history });
-            }}
-          >
-            {({ handleSubmit, isValid }) => {
-              return (
-                <Fragment>
-                  <Form
-                    className="flex flex-col items-center"
-                    onSubmit={handleSubmit}
-                  >
-                    <fieldset className="mb-5">
-                      <label className="block text-xs mb-2">Password</label>
-                      <Field
-                        placeholder="Enter Password"
-                        type="password"
-                        id="password"
-                        name="password"
-                        className="block w-72 text-xs p-3 border border-gray-400 rounded"
-                      />
-                    </fieldset>
+  return (
+    <section className="h-screen w-full setup-screens flex flex-col justify-center items-center leafy-bg">
+      <div className="flex flex-col items-center bg-white fadeIn login-fieldset auth-modal">
+        <div className="flex flex-col items-center setup-success mb-6">
+          <i className="w-10 mb-4">
+            <img src={Icon} alt="" />
+          </i>
+          <h1 className="text-xl font-medium mb-2">Set Password</h1>
+          <p className="text-gray-500 text-center leading-normal">
+            Enter a password that will be easy for you and difficult for others
+            to remember
+          </p>
+        </div>
 
-                    <fieldset className="mb-5">
-                      <label className="block text-xs mb-2">Password</label>
-                      <Field
-                        placeholder="Re-Enter Password"
-                        type="password"
-                        id="confirmPassword"
-                        name="confirmPassword"
-                        className="block w-72 text-xs p-3 border border-gray-400 rounded"
-                      />
-                    </fieldset>
+        {loading ? (
+          <Loading text="Verifying your password" />
+        ) : (
+          <Fragment>
+            {error && (
+              <div className="w-72 mb-8 text-xs text-left">
+                <p className="w-full p-3 bg-red-200 text-red-700 rounded text-center font-medium">
+                  {error}
+                </p>
+              </div>
+            )}
 
-                    <button
-                      type="submit"
-                      className="mt-6 w-40 text-center leading-loose bg-wb-primary wealth-buddy--cta text-white rounded-sm"
+            <Formik
+              initialValues={initialValues}
+              validationSchema={validationSchema}
+              validateOnMount={true}
+              onSubmit={handleOnSubmit}
+            >
+              {({ handleSubmit, isValid }) => {
+                return (
+                  <Fragment>
+                    <Form
+                      className="flex flex-col items-center"
                       onSubmit={handleSubmit}
-                      disabled={!isValid}
                     >
-                      Done
-                    </button>
-                  </Form>
+                      <fieldset className="mb-5">
+                        <label className="block text-xs mb-2">Password</label>
+                        <Field
+                          placeholder="Enter Password"
+                          type="password"
+                          id="password"
+                          name="password"
+                          className="block w-72 text-xs p-3 border border-gray-400 rounded"
+                        />
+                      </fieldset>
 
-                  <div className="mt-8 ">
-                    <p className="text-sm text-gray-500">
-                      Already have an account?{" "}
-                      <Link to="/auth/login" className="text-wb-primary">
-                        Sign In
-                      </Link>
-                    </p>
-                  </div>
-                </Fragment>
-              );
-            }}
-          </Formik>
-        </Fragment>
-      )}
-    </div>
-  </section>
-);
+                      <fieldset className="mb-5">
+                        <label className="block text-xs mb-2">Password</label>
+                        <Field
+                          placeholder="Re-Enter Password"
+                          type="password"
+                          id="confirmPassword"
+                          name="confirmPassword"
+                          className="block w-72 text-xs p-3 border border-gray-400 rounded"
+                        />
+                      </fieldset>
+
+                      <button
+                        type="submit"
+                        className="mt-6 w-40 text-center leading-loose bg-wb-primary wealth-buddy--cta text-white rounded-sm"
+                        onSubmit={handleSubmit}
+                        disabled={!isValid}
+                      >
+                        Done
+                      </button>
+                    </Form>
+
+                    <div className="mt-8 ">
+                      <p className="text-sm text-gray-500">
+                        Already have an account?{" "}
+                        <Link to="/auth/login" className="text-wb-primary">
+                          Sign In
+                        </Link>
+                      </p>
+                    </div>
+                  </Fragment>
+                );
+              }}
+            </Formik>
+          </Fragment>
+        )}
+      </div>
+    </section>
+  );
+};
 
 const mapStateToProps = (state) => ({
   loading: state.user.loginLoading,
   error: state.user.loginError,
+  verificationID: state.signUpParams.emailVerificationID,
 });
 
 const mapDispatchToProps = (dispatch) => ({
