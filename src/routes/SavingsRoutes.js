b5212c5879410c7a217bc77768b6ae55f42cf1c7
Abdulmatin Sanni <abdulmatin.i.sanni@gmail.com>
2020-08-15 15:25:49 +0100

    Refactor codes

--- src/routes/SavingsRoutes.js
+++ src/routes/SavingsRoutes.js
@@ -6,11 +6,8 @@ import CreateSavings from "pages/savings/CreateHome/CreateSavings";
 import PersonalSavings from "pages/savings/PersonalSavings/PersonalSavings";
 import FixedSavings from "pages/savings/FixedSavings/FixedSavings";
 import FixedFlexibleSavings from "pages/savings/FixedFlexibleSavings/FixedFlexibleSavings";
-import GroupSimple from "pages/savings/Group/simple";
 import CreateGroupTargetSavings from "pages/savings/groupTarget/CreateGroupTargetSavings";
 import CreateGroupChallengeSavings from "pages/savings/groupChallenge/CreateGroupChallengeSavings";
-
-const GroupContributory = () => <GroupSimple title="Group Contributory" />;
 
 const Savings = () => {
   let { path } = useRouteMatch();
@@ -24,7 +21,7 @@ const Savings = () => {
       <Route path={`${path}/create/fixed-flexible`} component={FixedFlexibleSavings} />
       <Route path={`${path}/create/group-target`} component={CreateGroupTargetSavings} />
       <Route path={`${path}/create/group-challenge`} component={CreateGroupChallengeSavings} />
-      <Route path={`${path}/create/group-contributory`} component={GroupContributory} />
+      {/*<Route path={`${path}/create/group-contributory`} component={GroupContributory} />*/}
       <Route path={`${path}/view/:savingsId`} component={ViewSavings} />
     </Switch>
   );
