d42fc38acd72f8c3b635fb3de892c17027957e35
Tega Oboraruvwe <tegararuvwe@gmail.com>
2020-08-19 09:59:11 +0100

    bug fixed

--- src/pages/investment/components/myInvestment/MyInvestment.js
+++ src/pages/investment/components/myInvestment/MyInvestment.js
@@ -17,56 +17,58 @@ const MyInvestment = (props) => {
 
   return props.allPersonalInvestmentsLoading ? null : (
     <div className="flex flex-row flex-wrap  scroll-container ps">
-      {props.allPersonalInvestmentsData.map((item, index) => {
-        return (
-          <Link
-            key={index}
-            to={{
-              pathname: "/dashboard/investment/view-investment",
-              investmentId: item.securityId,
-            }}
-            style={{ border: " 1px solid #F1F1F1", borderRadius: "2px" }}
-            className="card px-4 py-4  flex mr-4 flex-col"
-          >
-            <div className="flex flex-row content-center items-center">
-              <img
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ8CyHlXfQ0X5KJ_kj1pRohugCUtBom9Qk1wg&usqp=CAU"
-                alt=""
-                className="rounded-sm"
-                style={{ height: "78.41px", width: "90.71px" }}
-              />
-              <p
-                style={{ width: "100px" }}
-                className="text-black font-bold text-base ml-2 "
+      {!props.allPersonalInvestmentsData
+        ? null
+        : props.allPersonalInvestmentsData.map((item, index) => {
+            return (
+              <Link
+                key={index}
+                to={{
+                  pathname: "/dashboard/investment/view-investment",
+                  investmentId: item.securityId,
+                }}
+                style={{ border: " 1px solid #F1F1F1", borderRadius: "2px" }}
+                className="card px-4 py-4  flex mr-4 flex-col"
               >
-                {item.companyName}
-              </p>
-            </div>
+                <div className="flex flex-row content-center items-center">
+                  <img
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ8CyHlXfQ0X5KJ_kj1pRohugCUtBom9Qk1wg&usqp=CAU"
+                    alt=""
+                    className="rounded-sm"
+                    style={{ height: "78.41px", width: "90.71px" }}
+                  />
+                  <p
+                    style={{ width: "100px" }}
+                    className="text-black font-bold text-base ml-2 "
+                  >
+                    {item.companyName}
+                  </p>
+                </div>
 
-            <div
-              style={{ borderTop: "1px solid #F1F1F1" }}
-              className="flex flex-row mt-2 pt-2 justify-between"
-            >
-              <div>
-                <p className="text-black text-sm">Current Value</p>
-                <p className="text-black mt-1 font-bold text-base">
-                  N {formatCurrency(item.currentValue.toFixed(2))}
-                </p>
-              </div>
-
-              <div>
-                <p className="text-black text-sm">Returns</p>
-                <p
-                  style={{ color: "#8CB13D" }}
-                  className="font-bold mt-1 text-right text-base"
+                <div
+                  style={{ borderTop: "1px solid #F1F1F1" }}
+                  className="flex flex-row mt-2 pt-2 justify-between"
                 >
-                  {item.portPercentage.toFixed(2)}%
-                </p>
-              </div>
-            </div>
-          </Link>
-        );
-      })}
+                  <div>
+                    <p className="text-black text-sm">Current Value</p>
+                    <p className="text-black mt-1 font-bold text-base">
+                      N {formatCurrency(item.currentValue.toFixed(2))}
+                    </p>
+                  </div>
+
+                  <div>
+                    <p className="text-black text-sm">Returns</p>
+                    <p
+                      style={{ color: "#8CB13D" }}
+                      className="font-bold mt-1 text-right text-base"
+                    >
+                      {item.portPercentage.toFixed(2)}%
+                    </p>
+                  </div>
+                </div>
+              </Link>
+            );
+          })}
 
       {props.investmentValuationLoading
         ? null
