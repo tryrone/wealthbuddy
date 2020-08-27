0cfe4b746ed0dfd18d866714b4500cab3bdf4aef
Abayomi <roshbon@gmail.com>
2020-08-27 23:12:07 +0100

    added analytics and pie chart for dashboard

--- src/pages/dashboard/components/DashboardInner/DashboardAnalysis.js
+++ src/pages/dashboard/components/DashboardInner/DashboardAnalysis.js
@@ -1,82 +1,83 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import EmptyCard from "./EmptyCard";
 import ReactApexChart from "react-apexcharts";
+import Chart from 'chart.js';
 
 const DashboardAnalysis = () => {
-  const [state] = useState({
-    series: [
-      {
-        name: "series1",
-        data: [31, 40, 28, 51, 42, 109, 100],
-      },
-      {
-        name: "series2",
-        data: [11, 32, 45, 32, 34, 52, 41],
-      },
-    ],
-    colors: ["#ef878e", "#3986ee"],
-    options: {
-      chart: {
-        height: "100%",
-        width: "100%",
-        type: "area",
-        toolbar: {
-          show: false,
+  const [chartOptions] = useState({
+    type: 'line',
+    data: {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July' ],
+      datasets: [
+        {
+          label: 'Savings',
+          data: [40000, 644500, 30000, 50000, 35000, 15000, 50000],
+          backgroundColor: "#fa90a2",
+          borderColor: '#fa90a2',
+          fill: false
         },
-      },
-      dataLabels: {
-        enabled: false,
-      },
-      stroke: {
-        curve: "smooth",
-        colors: ["#ef878e", "#3986ee"],
-        width: 2,
-      },
-      markers: {
-        colors: ["#ef878e", "#3986ee"],
-      },
-      fill: {
-        colors: ["#d9e8fC", "#ffffff"],
-      },
-      legend: {
-        labels: {
-          colors: ["#ef878e", "#3986ee"],
-          // useSeriesColors: true
+        {
+          label: 'Investment',
+          data: [25000, 30000, 450000, 32000, 45000, 520000, 1000000],
+          backgroundColor: '#cf67f4',
+          borderColor: '#cf67f4',
+          fill: false
         },
-        markers: {
-          fillColors: ["#ef878e", "#3986ee"],
-          // useSeriesColors: true
-        },
-      },
-      xaxis: {
-        type: "datetime",
-        categories: [
-          "2018-09-19T00:00:00.000Z",
-          "2018-09-19T01:30:00.000Z",
-          "2018-09-19T02:30:00.000Z",
-          "2018-09-19T03:30:00.000Z",
-          "2018-09-19T04:30:00.000Z",
-          "2018-09-19T05:30:00.000Z",
-          "2018-09-19T06:30:00.000Z",
-        ],
-      },
-      tooltip: {
-        x: {
-          format: "dd/MM/yy HH:mm",
-        },
-      },
+      ]
     },
+    options: {
+      responsive: true,
+      title: {
+        display: false,
+        text: 'Title'
+      },
+      tooltips: {
+        mode: 'index',
+        intersect: false,
+      },
+      hover: {
+        mode: 'nearest',
+        intersect: true
+      },
+      scales: {
+        x: {
+          display: true,
+          scaleLabel: {
+            display: true,
+            labelString: 'Month'
+          }
+        },
+        y: {
+          display: true,
+          scaleLabel: {
+            display: true,
+            labelString: 'Value'
+          }
+        }
+      }
+    }
   });
+  const canvasEl = useRef(null);
+
+  useEffect(() => {
+    if (canvasEl.current instanceof HTMLCanvasElement) {
+      new Chart(canvasEl.current, chartOptions);
+    }
+    return () => {
+    }
+  }, [canvasEl, Chart, chartOptions])
 
   return (
     <div className="flex flex-col card w-full">
-      <h1 className="text-4xl mb-6 font-medium card-header">Analytics</h1>
-      <div className="flex-grow flex justify-center items-center">
-        <EmptyCard
+      <h1 className="text-4xl mb-4 font-medium card-header">Analytics</h1>
+      <div className="flex-grow flex justify-center items-center px-20">
+        {/* <EmptyCard
           title="Nothing to see here yet."
           message="Find any of your savings plan to and see you your transactions history here."
-        />
-        {/*<ReactApexChart*/}
+        /> */}
+        <canvas ref={canvasEl} />
+        {/*
+        <can<ReactApexChart*/}
         {/*    className="gggggg"*/}
         {/*  options={state.options}*/}
         {/*  series={state.series}*/}
