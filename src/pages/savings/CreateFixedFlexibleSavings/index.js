bf59e6e61314b0f3b85acfb49fde3e0ae3399182
Abdulmatin Sanni <abdulmatin.i.sanni@gmail.com>
2020-09-05 20:57:05 +0100

    Add funding option to create fixed flexible savings

--- src/pages/savings/CreateFixedFlexibleSavings/index.js
+++ src/pages/savings/CreateFixedFlexibleSavings/index.js
@@ -9,6 +9,7 @@ import DisclaimerModal from "./components/DisclaimerModal";
 import { createFixedFlexibleSavings } from "state/slices/savings";
 import moment from "moment";
 import CreateSavingsSuccessModal from "./components/CreateSavingsSuccessModal";
+import FundSavingsModal from "../CreateFixedLockSavings/components/FundSavingsModal";
 
 const CreateFixedFlexibleSavings = ({ savingsConfiguration }) => {
   const dispatch = useDispatch();
@@ -21,6 +22,7 @@ const CreateFixedFlexibleSavings = ({ savingsConfiguration }) => {
   const [state, setState] = useState({
     showCreationPage: true,
     showConfirmationPage: false,
+    showFundSavingsModal: false,
     showDisclaimerModal: false,
     showCreateSavingsSuccessModal: false,
     isCreateLoading: false,
@@ -32,6 +34,8 @@ const CreateFixedFlexibleSavings = ({ savingsConfiguration }) => {
       applyInterest: true,
       file: "",
       imagePreviewUrl: null,
+      allowCardDebit: null,
+      cardId: "",
     },
   });
 
@@ -59,8 +63,29 @@ const CreateFixedFlexibleSavings = ({ savingsConfiguration }) => {
     e.preventDefault();
     setState(
       produce((draft) => {
-        draft.showDisclaimerModal = true;
+        draft.formValues.allowCardDebit = null;
+        draft.formValues.cardId = "";
+        draft.showFundSavingsModal = true;
       })
+    );
+  };
+
+  const handleCloseFundSavingsModal = () => {
+    setState(
+        produce((draft) => {
+          draft.showFundSavingsModal = false;
+        })
+    );
+  };
+
+  const handleSubmitFundSavingsForm = ({ cardId, allowCardDebit }) => {
+    setState(
+        produce((draft) => {
+          draft.showFundSavingsModal = false;
+          draft.showDisclaimerModal = true;
+          draft.formValues.allowCardDebit = allowCardDebit;
+          draft.formValues.cardId = cardId;
+        })
     );
   };
 
@@ -86,6 +111,8 @@ const CreateFixedFlexibleSavings = ({ savingsConfiguration }) => {
       amountToSave: state.formValues.amount,
       MaturityDate: moment(state.formValues.maturityDate).toISOString(),
       ApplyInterest: state.formValues.applyInterest,
+      allowCardDebit: state.formValues.allowCardDebit,
+      cardId: state.formValues.cardId,
     };
 
     const formData = new FormData();
@@ -141,6 +168,13 @@ const CreateFixedFlexibleSavings = ({ savingsConfiguration }) => {
           isVisible={state.showConfirmationPage}
           onBack={handleClickBackOnConfirmationPage}
           onLaunch={handleClickLaunchOnConfirmationPage}
+        />
+
+        <FundSavingsModal
+            formValues={state.formValues}
+            isVisible={state.showFundSavingsModal}
+            onSubmit={handleSubmitFundSavingsForm}
+            close={handleCloseFundSavingsModal}
         />
 
         <DisclaimerModal
